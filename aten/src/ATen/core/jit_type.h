b926f095014942295d68c9393b9b8371c0806be6
xuewenc <xuewenc@devvm613.frc0.facebook.com>
2020-05-18 21:49:20 -0700

    [JIT]  JIT should let people know we inferred an argument as a tensor
    Summary:
    
    This PR solves issue (#37200).
    Error is encountered during IR generation while trying to resolve the call to sum.
    Should let user know it inferred the value for argument 'dim' to be of type 'Tensor'
    because it was not annotated with an explicit type.
    
    Test Plan:
    
    Add code to reprodue the issue (#37200)
    `python test/test_jit.py TestJit.test_inferred_as_tensor`
    
    Reviewers:
    meghanl,suo
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 1f2c29161fc795a57280a967e7fed3bfb1ae7a20
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/38527

--- aten/src/ATen/core/jit_type.h
+++ aten/src/ATen/core/jit_type.h
@@ -661,6 +661,17 @@ struct CAFFE2_API TensorType : public Type {
     return scalar_type_ && device_ && sizes_.isComplete() && strides_.isComplete();
   }
 
+  bool isInferredType() const {
+    return is_inferred_;
+  }
+
+  static TensorTypePtr getInferred() {
+    auto valueInferred = TensorType::create(
+      {}, {}, VaryingShape<ShapeSymbol>{}, VaryingShape<Stride>{}, {});
+    valueInferred->is_inferred_ = true;
+    return valueInferred;
+  }
+
   // this property is used by GuardElimination
   // please see `checkInputs` for more details
   bool isSummarized() const {
@@ -732,6 +743,8 @@ struct CAFFE2_API TensorType : public Type {
   // defined and undefined. However, no tensor type starts out with
   // `undefined_` set to `c10::nullopt`
   c10::optional<bool> undefined_;
+  // Represents whether or not this type was inferred.
+  bool is_inferred_ = false;
 };
 
 struct ListType;
